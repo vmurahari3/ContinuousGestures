87502a4a860afb2e78d84ba78ec73c914523cbf7
Aman Parnami <amanparnami@gmail.com>
2017-04-22 13:03:01 -0400

    [ADD] TestingActivity added

--- app/src/main/java/edu/gatech/ubicomp/continuousgestures/TestingActivity.java
+++ app/src/main/java/edu/gatech/ubicomp/continuousgestures/TestingActivity.java
@@ -22,8 +22,8 @@ import javax.inject.Inject;
 import edu.gatech.ubicomp.continuousgestures.data.learning.Segmenter;
 import io.realm.Realm;
 
-public class MainActivity extends Activity implements SensorEventListener {
-    private static final String TAG = MainActivity.class.getSimpleName();
+public class TestingActivity extends Activity implements SensorEventListener {
+    private static final String TAG = TestingActivity.class.getSimpleName();
 
     @Inject
     public RxEventBus mRxEventBus;
@@ -38,20 +38,20 @@ public class MainActivity extends Activity implements SensorEventListener {
     boolean flag_sound_generated = false;
     public static int  buffer_size = 10;
     public static final double threshold = 0.2;
-    private ArrayList<ArrayList<Double>>  sensor_values = new ArrayList<>();
+    private ArrayList<ArrayList<Double>> sensorValues = new ArrayList<>();
 
-    private ArrayList<ArrayList<Double>>  possible_gesture_data = new ArrayList<>();
-    private ArrayList<Long> possible_gesture_data_timestamp = new ArrayList<>();
+    private ArrayList<ArrayList<Double>> possibleGestureData = new ArrayList<>();
+    private ArrayList<Long> possibleGestureDataTimestamp = new ArrayList<>();
 
-    private ArrayList<Long> gyro_timestamp = new ArrayList<>();
+    private ArrayList<Long> gyroTimestamp = new ArrayList<>();
 
     private boolean first_fire = false;
-    private long prev_fire_timestamp, cur_timestamp;
+    private long prev_fire_timestamp, curTimestamp;
     private DataAnalyzer mDataAnalyzer;
 
     private HashMap<String, String> labelNameMap = new HashMap<String, String>();
-    private boolean is_gesture_executing = false;
-    private int overlap_control = 0;
+    private boolean isGestureExecuting = false;
+    private int overlapControl = 0;
 
     private TextView resultTV;
 
@@ -59,7 +59,7 @@ public class MainActivity extends Activity implements SensorEventListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(edu.gatech.ubicomp.continuousgestures.R.layout.activity_main);
+        setContentView(edu.gatech.ubicomp.continuousgestures.R.layout.activity_testing);
         DependencyUtil.inject(this, this);
 
         // initialize sensor stuff
@@ -69,8 +69,8 @@ public class MainActivity extends Activity implements SensorEventListener {
 
         mDataAnalyzer = new DataAnalyzer(this);
         for (int i  = 0; i<6 ; i++){
-            sensor_values.add(new ArrayList<Double>());
-            possible_gesture_data.add(new ArrayList<Double>());
+            sensorValues.add(new ArrayList<Double>());
+            possibleGestureData.add(new ArrayList<Double>());
         }
         //@todo hardcoded here might need to change as gesture classes increase
         labelNameMap.put("10", "Clockwise");
@@ -79,12 +79,6 @@ public class MainActivity extends Activity implements SensorEventListener {
         labelNameMap.put("13", "Not detected");
 
         resultTV = (TextView) findViewById(edu.gatech.ubicomp.continuousgestures.R.id.resultTV);
-
-        // initialize classification stuff
-        mDataAnalyzer.init();
-        // load arff file
-        mDataAnalyzer.loadArffFile("file:///android_asset/null_class_model_final.arff");
-        mDataAnalyzer.buildClassifier();
 
         // Create the Realm instance
         realm = Realm.getDefaultInstance();
@@ -113,21 +107,21 @@ public class MainActivity extends Activity implements SensorEventListener {
 
         // store in a buffer
         int type = event.sensor.getType();
-        cur_timestamp = event.timestamp;
+        curTimestamp = event.timestamp;
 
         if (type == Sensor.TYPE_LINEAR_ACCELERATION) {
 
             float[] acceleration = event.values;
             for(int i = 0; i<acceleration.length; i++ ){
-                sensor_values.get(i).add((double)acceleration[i]);
-                if(is_gesture_executing) {
-                    possible_gesture_data.get(i).add((double)acceleration[i]);
+                sensorValues.get(i).add((double)acceleration[i]);
+                if(isGestureExecuting) {
+                    possibleGestureData.get(i).add((double)acceleration[i]);
 
                 }
             }
-            if(sensor_values.get(0).size() > Constants.WIN_SIZE){
+            if(sensorValues.get(0).size() > Constants.WIN_SIZE){
                 for(int i =0; i< acceleration.length; i++){
-                    sensor_values.get(i).remove(0);
+                    sensorValues.get(i).remove(0);
                 }
             }
 
@@ -135,72 +129,72 @@ public class MainActivity extends Activity implements SensorEventListener {
         } else if (type == Sensor.TYPE_GYROSCOPE) {
 
             float[] gyro = event.values;
-            gyro_timestamp.add(cur_timestamp);
-            // overlap_control helps us calculate energy only at certain times
-            overlap_control = (overlap_control + 1) % Constants.OVERLAP;
+            gyroTimestamp.add(curTimestamp);
+            // overlapControl helps us calculate energy only at certain times
+            overlapControl = (overlapControl + 1) % Constants.OVERLAP;
 
             for(int i = 0; i<gyro.length; i++ ){
-                sensor_values.get(i+3).add((double)gyro[i]);
-                if(is_gesture_executing) {
-                    possible_gesture_data.get(i+3).add((double)gyro[i]);
-                    possible_gesture_data_timestamp.add(cur_timestamp);
+                sensorValues.get(i+3).add((double)gyro[i]);
+                if(isGestureExecuting) {
+                    possibleGestureData.get(i+3).add((double)gyro[i]);
+                    possibleGestureDataTimestamp.add(curTimestamp);
                 }
             }
-            if(sensor_values.get(3).size() > Constants.WIN_SIZE){
+            if(sensorValues.get(3).size() > Constants.WIN_SIZE){
                 for(int i =0; i< gyro.length; i++){
-                    sensor_values.get(i+3).remove(0);
+                    sensorValues.get(i+3).remove(0);
                 }
                 // also trim the timestamp list
-                gyro_timestamp.remove(0);
+                gyroTimestamp.remove(0);
             }
         }
 
-//        if( sensor_values.get(0).size() == Constants.WIN_SIZE && sensor_values.get(3).size() == Constants.WIN_SIZE) {
+//        if( sensorValues.get(0).size() == Constants.WIN_SIZE && sensorValues.get(3).size() == Constants.WIN_SIZE) {
 //            // fire classifier
 //            if (!first_fire) {
 //                first_fire = true;ArrayList<ArrayList<Double>>
 //                prev_fire_timestamp = event.timestamp;
-//                fire_classifier();
+//                fireClassifier();
 //            }
 //            else{
 //                // 10 percent overlap for a 3 sec window
-//                if (( cur_timestamp - prev_fire_timestamp) > Constants.SLIDE_SIZE){
-//                    fire_classifier();
-//                    prev_fire_timestamp = cur_timestamp;
+//                if (( curTimestamp - prev_fire_timestamp) > Constants.SLIDE_SIZE){
+//                    fireClassifier();
+//                    prev_fire_timestamp = curTimestamp;
 //                }
 //            }
 
 //        }
 
-//        Log.d(TAG, "size of gyro sensor values is " + sensor_values.get(0).size());
+//        Log.d(TAG, "size of gyro sensor values is " + sensorValues.get(0).size());
         // detection code
 
-        if(!is_gesture_executing && overlap_control == 0){
+        if(!isGestureExecuting && overlapControl == 0){
 
-            if( Segmenter.segment_start_test(sensor_values, gyro_timestamp)){
+            if( Segmenter.segmentStartTest(sensorValues, gyroTimestamp)){
 
-                is_gesture_executing = true;
+                isGestureExecuting = true;
 
             }
             // deleting previous data in possible gesture data
-            possible_gesture_data = new ArrayList<>();
+            possibleGestureData = new ArrayList<>();
             for (int i  = 0; i<6 ; i++){
-                possible_gesture_data.add(new ArrayList<Double>());
+                possibleGestureData.add(new ArrayList<Double>());
             }
-            possible_gesture_data_timestamp.clear();
+            possibleGestureDataTimestamp.clear();
 
         }
-        else if(is_gesture_executing && overlap_control == 0){
-            if(Segmenter.segment_stop_test(sensor_values, gyro_timestamp)){
+        else if(isGestureExecuting && overlapControl == 0){
+            if(Segmenter.segmentStopTest(sensorValues, gyroTimestamp)){
 
-                is_gesture_executing = false;
+                isGestureExecuting = false;
                 // do gesture pruning here. test whether the length of the gesture is greater than 0.4 seconds
                 // if so fire classifier with that data
                 // the pruning represent about 0.4 seconds of data
 
-                Log.d(TAG, " condition: " + ( possible_gesture_data_timestamp.get(possible_gesture_data_timestamp.size()-1) - possible_gesture_data_timestamp.get(0))/Math.pow(10,9) );
-                if(( possible_gesture_data_timestamp.get(possible_gesture_data_timestamp.size()-1) - possible_gesture_data_timestamp.get(0) )> 40 * Math.pow(10,7)){
-                    fire_classifier(possible_gesture_data);
+                Log.d(TAG, " condition: " + ( possibleGestureDataTimestamp.get(possibleGestureDataTimestamp.size()-1) - possibleGestureDataTimestamp.get(0))/Math.pow(10,9) );
+                if(( possibleGestureDataTimestamp.get(possibleGestureDataTimestamp.size()-1) - possibleGestureDataTimestamp.get(0) )> 40 * Math.pow(10,7)){
+                    fireClassifier(possibleGestureData);
                 }
 
             }
@@ -211,17 +205,16 @@ public class MainActivity extends Activity implements SensorEventListener {
     public void onAccuracyChanged(Sensor sensor, int accuracy){
 
     }
-    private void fire_classifier(ArrayList<ArrayList<Double>> gesture_data){
-        ArrayList<Double[]> ret = new ArrayList<>();
-        for(ArrayList<Double> ch : gesture_data ){
-            ret.add(ch.toArray(new Double[ch.size()]));
-        }
-        Log.d(TAG, "fired classifier");
-        mDataAnalyzer.addTestSample(ret);
-        String label = mDataAnalyzer.classifyGesture();
-        String name = labelNameMap.get(label);
-        Log.d(TAG, "result: " + name);
-        resultTV.setText(name);
+    private void fireClassifier(ArrayList<ArrayList<Double>> gestureData){
+//        ArrayList<Double[]> ret = new ArrayList<>();
+//        for(ArrayList<Double> ch : gestureData ){
+//            ret.add(ch.toArray(new Double[ch.size()]));
+//        }
+//        Log.d(TAG, "fired classifier");
+//        String label = mDataAnalyzer.classifyTestSample(ret);
+//        String name = labelNameMap.get(label);
+//        Log.d(TAG, "result: " + name);
+//        resultTV.setText(name);
     }
 
 }
